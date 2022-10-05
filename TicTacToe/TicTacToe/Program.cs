d5140defa864ddd4cd2b90a31c1ce0f566a34032
ra74-2018 <nikolajovisic4@gmail.com>
2022-10-05 10:46:37 +0200

    feat: initial simple main

---
+++ TicTacToe/TicTacToe/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game(3);
+            GameUI UI = new GameUI(game);
+            UI.Play();
+        }
+    }
+}
