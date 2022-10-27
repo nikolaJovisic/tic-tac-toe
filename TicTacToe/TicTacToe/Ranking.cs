32320105fbb6559210cd00eaa83830e8fd5133ef
NikolaLugumerski <nikolalugumerski123@gmail.com>
2022-10-27 08:43:54 +0200

    Ranking UI fixed

--- TicTacToe/TicTacToe/Ranking.cs
+++ TicTacToe/TicTacToe/Ranking.cs
@@ -38,18 +38,21 @@ namespace TicTacToe
         {
             var ranking = scores.OrderBy(x => -x.Value.Points).ToList();
 
-            Console.WriteLine("ranking, name, points, played");
+            Console.WriteLine("HIGH SCORES");
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine(" RANKING | NAME | POINTS | PLAYED |");
+			Console.WriteLine();
 
             for (int i = 0; i < ranking.Count(); ++i)
             {
                 var entry = ranking.ElementAt(i);
                 if (entry.Key.Equals(name))
                 {
-                    Console.WriteLine(string.Format("{0}, {1}, {2}, {3} <------- you", i + 1, name, entry.Value.Points, entry.Value.GamesPlayed));
+                    Console.WriteLine(string.Format(" #{0,-7}|{1,-6}|{2,-8}|{3,-8}| <----- you", i + 1, name, entry.Value.Points, entry.Value.GamesPlayed));
                 }
-                else if (i < Console.WindowHeight - 4)
+                else if (i < Console.WindowHeight - 7)
                 {
-                    Console.WriteLine(string.Format("{0}, {1}, {2}, {3}", i + 1, entry.Key, entry.Value.Points, entry.Value.GamesPlayed));
+                    Console.WriteLine(string.Format(" #{0,-7}|{1,-6}|{2,-8}|{3,-8}|", i + 1, name, entry.Key, entry.Value.Points, entry.Value.GamesPlayed));
                 }
             }
             Console.WriteLine("Press any key to continue...");
