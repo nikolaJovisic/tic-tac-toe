f47d6ade04c56a81a4e9a89acc3b71e3604e9819
ra74-2018 <115583370+nikola-diffine@users.noreply.github.com>
2022-11-09 09:57:37 +0100

    sort by sos test (red)

---
+++ TicTacToe/TicTacToeTests/RankingTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TicTacToe;
+using Xunit;
+
+namespace TicTacToeTests
+{
+    public class RankingTests
+    {
+        [Fact]
+        public void Basic()
+        {
+            Ranking ranking = new Ranking();
+            ranking.WinnedGameFinishedUpdate("p2", "p1");
+            var sorted = ranking.SortedScores();
+            Assert.Equal("p2", sorted[0].Name);
+            Assert.Equal("p1", sorted[1].Name);
+        }
+
+        [Fact]
+        public void SortBySOS()
+        {
+            Ranking ranking = new Ranking();
+            ranking.WinnedGameFinishedUpdate("p1", "p2");
+            ranking.WinnedGameFinishedUpdate("p1", "p3");
+            ranking.WinnedGameFinishedUpdate("p2", "p4");
+            ranking.WinnedGameFinishedUpdate("p3", "p2");
+
+            var sorted = ranking.SortedScores();
+
+            Assert.Equal("p1", sorted[0].Name);
+            Assert.Equal("p3", sorted[1].Name);
+            Assert.Equal("p2", sorted[2].Name);
+            Assert.Equal("p4", sorted[3].Name);
+        }
+    }
+}
