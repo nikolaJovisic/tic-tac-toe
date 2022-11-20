59d2ea2b8691059c77c0c3f4844f864d550f7ea6
ra74-2018 <115583370+nikola-diffine@users.noreply.github.com>
2022-11-20 13:17:12 +0100

    add player score metrics for build not to fail at tests

--- TicTacToe/TicTacToe/PlayerScore.cs
+++ TicTacToe/TicTacToe/PlayerScore.cs
@@ -11,6 +11,10 @@ namespace TicTacToe
         int points = 0;
         int gamesPlayed = 0;
         int rank = -1;
+        private int sumOpponentScores;
+        private int sumDefeatedOpponentScores;
+        private int sumOpponentSOS;
+        private int sumOpponentDOS;
 
         public int GamesPlayed { get => gamesPlayed; }
         public string Name { get => name; }
@@ -19,6 +23,11 @@ namespace TicTacToe
         public int Rank { set => rank = value; get => rank; }
 
         public int Points { set => points = value; get => points; }
+
+        public int SumOpponentScores { set => sumOpponentScores = value; get => sumOpponentScores; }
+        public int SumDefeatedOpponentScores { set => sumDefeatedOpponentScores = value; get => sumDefeatedOpponentScores; }
+        public int SumOpponentSOS { set => sumOpponentSOS = value; get => sumOpponentSOS; }
+        public int SumOpponentDOS { set => sumOpponentDOS = value; get => sumOpponentDOS; }
 
         public PlayerScore(string name)
         {
