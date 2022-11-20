ab9a8110ac1aa014a693b40de9dc00c956a14ac1
ra74-2018 <115583370+nikola-diffine@users.noreply.github.com>
2022-11-20 13:41:52 +0100

    sodos sort test (fails)

--- TicTacToe/TicTacToeTests/RankingTests.cs
+++ TicTacToe/TicTacToeTests/RankingTests.cs
@@ -187,5 +187,34 @@ namespace TicTacToeTests
             Assert.Equal(10, rankingScores["p1"].SumOpponentDOS);
         }
 
+        [Fact]
+        public void SODOSSort()
+        {
+            string p1 = "p1";
+            string p2 = "p2";
+            Dictionary<string, PlayerScore> scores = new Dictionary<string, PlayerScore>();
+
+            PlayerScore s1 = new PlayerScore(p1);
+            s1.Points = 10;
+            s1.SumOpponentScores = 10;
+            s1.SumDefeatedOpponentScores = 10;
+            s1.SumOpponentSOS = 10;
+            s1.SumOpponentDOS = 0;
+            PlayerScore s2 = new PlayerScore(p2);
+            s2.Points = 10;
+            s2.SumOpponentScores = 10;
+            s2.SumDefeatedOpponentScores = 10;
+            s2.SumOpponentSOS = 10;
+            s2.SumOpponentDOS = 10;
+
+            scores.Add(p1, s1);
+            scores.Add(p2, s2);
+
+            var sortedScores = ScoreSorter.Sort(scores.Values);
+
+            Assert.Equal("p2", sortedScores[0].Name);
+            Assert.Equal("p1", sortedScores[1].Name);
+        }
+
     }
 }
