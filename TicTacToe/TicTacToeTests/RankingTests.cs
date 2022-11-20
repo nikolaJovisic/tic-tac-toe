c4f064c10fa21d6333be0302d473436f4b5e3da7
ra74-2018 <115583370+nikola-diffine@users.noreply.github.com>
2022-11-20 13:31:21 +0100

    sdos test (fails)

--- TicTacToe/TicTacToeTests/RankingTests.cs
+++ TicTacToe/TicTacToeTests/RankingTests.cs
@@ -87,5 +87,30 @@ namespace TicTacToeTests
             Assert.Equal(20, rankingScores["p1"].SumDefeatedOpponentScores);
         }
 
+        [Fact]
+        public void SDOSSort()
+        {
+            string p1 = "p1";
+            string p2 = "p2";
+            Dictionary<string, PlayerScore> scores = new Dictionary<string, PlayerScore>();
+
+            PlayerScore s1 = new PlayerScore(p1);
+            s1.Points = 10;
+            s1.SumOpponentScores = 10;
+            s1.SumDefeatedOpponentScores = 0;
+            PlayerScore s2 = new PlayerScore(p2);
+            s2.Points = 10;
+            s2.SumOpponentScores = 0;
+            s2.SumDefeatedOpponentScores = 10;
+
+            scores.Add(p1, s1);
+            scores.Add(p2, s2);
+
+            var sortedScores = ScoreSorter.Sort(scores.Values);
+
+            Assert.Equal("p2", sortedScores[0].Name);
+            Assert.Equal("p1", sortedScores[1].Name);
+        }
+
     }
 }
