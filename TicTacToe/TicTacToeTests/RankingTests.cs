28de92972e45d86322d7a33f6f2918d4204eafa9
ra74-2018 <115583370+nikola-diffine@users.noreply.github.com>
2022-11-20 13:28:09 +0100

    sdos update test (fails)

--- TicTacToe/TicTacToeTests/RankingTests.cs
+++ TicTacToe/TicTacToeTests/RankingTests.cs
@@ -64,5 +64,28 @@ namespace TicTacToeTests
             Assert.Equal("p1", sortedScores[1].Name);
         }
 
+        [Fact]
+        public void SDOSUpdate()
+        {
+            string p1 = "p1";
+            string p2 = "p2";
+            Dictionary<string, PlayerScore> scores = new Dictionary<string, PlayerScore>();
+
+            PlayerScore s1 = new PlayerScore(p1);
+            s1.Points = 10;
+            PlayerScore s2 = new PlayerScore(p2);
+            s2.Points = 20;
+
+            scores.Add(p1, s1);
+            scores.Add(p2, s2);
+
+            Ranking ranking = new Ranking(scores);
+
+            ranking.FinishGameWinnerLoser("p1", "p2");
+
+            var rankingScores = ranking.Scores;
+            Assert.Equal(20, rankingScores["p1"].SumDefeatedOpponentScores);
+        }
+
     }
 }
