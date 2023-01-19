8cec7b6b771ecb364a4a2e8dea78a6680b3e26eb
NikolaLugumerski <nikolalugumerski123@gmail.com>
2023-01-19 17:58:02 +0100

    Multiplayer Inital projects

---
+++ TicTacToe/TicTacToeContract/Utility/MultiplayerData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToeContract.Utility
+{
+	public class MultiplayerData
+	{
+		public string x { get; set; }
+	}
+}
